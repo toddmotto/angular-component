8d864c6f8526ca155c2c1f8c2d4fb0159b0b5dbe
Ryan Clark <ryan@ryanclark.me>
2016-05-21 21:43:27 +0100

    feat(karma): Add Karma testing configuration

---
+++ karma.conf.js
@@ -0,0 +1,41 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '.',
+    frameworks: ['jasmine'],
+    files: [
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'src/angular-component.js',
+      'tests/**/*.spec.js'
+    ],
+    exclude: [],
+    plugins: [
+      require('karma-chrome-launcher'),
+      require('karma-jasmine'),
+      require('karma-babel-preprocessor'),
+      require('karma-spec-reporter'),
+      require('karma-junit-reporter')
+    ],
+    preprocessors: {
+      'src/js/**/*.js': ['babel']
+    },
+    babelPreprocessor: {
+      options: {
+        presets: ['es2015']
+      }
+    },
+    reporters: ['spec', 'junit'],
+    junitReporter: {
+      outputDir: (process.env.CIRCLE_TEST_REPORTS || 'tests') + '/',
+      outputFile: 'junit.xml',
+      useBrowserName: false
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: false,
+    browsers: ['Chrome'],
+    singleRun: true,
+    concurrency: Infinity
+  })
+};
