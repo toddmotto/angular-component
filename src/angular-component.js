8c122f8a8d2e9b08e6c931bed5356c966a87e293
Mikhail Mordvinkin <mordvinkinm@yandex.ru>
2016-06-05 23:40:29 +0300

    Tabs replaced by spaces (2x indentation)

--- src/angular-component.js
+++ src/angular-component.js
@@ -122,19 +122,19 @@
               if (oneWayQueue.length) {
                 var destroyQueue = [];
                 for (var q = oneWayQueue.length; q--;) {
-				  (function(current){
-					  var unbindParent = $scope.$parent.$watch($attrs[current.attr], function (newValue, oldValue) {
-						self[current.local] = newValue;
-						updateChangeListener(current.local, newValue, oldValue, true);
-					  });
-					  destroyQueue.unshift(unbindParent);
-					  var unbindLocal = $scope.$watch(function () {
-						return self[current.local];
-					  }, function (newValue, oldValue) {
-						updateChangeListener(current.local, newValue, oldValue, false);
-					  });
-					  destroyQueue.unshift(unbindLocal);
-				  })(oneWayQueue[q]);
+                  (function(current){
+                    var unbindParent = $scope.$parent.$watch($attrs[current.attr], function (newValue, oldValue) {
+                      self[current.local] = newValue;
+                      updateChangeListener(current.local, newValue, oldValue, true);
+                    });
+                    destroyQueue.unshift(unbindParent);
+                    var unbindLocal = $scope.$watch(function () {
+                      return self[current.local];
+                    }, function (newValue, oldValue) {
+                      updateChangeListener(current.local, newValue, oldValue, false);
+                    });
+                    destroyQueue.unshift(unbindLocal);
+                  })(oneWayQueue[q]);
                 }
                 $scope.$on('$destroy', function () {
                   for (var i = destroyQueue.length; i--;) {
